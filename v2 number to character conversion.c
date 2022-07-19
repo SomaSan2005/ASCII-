3bef2a9fa20cac1f670d329ccd99a75efdf558cc
SomaSan2005 <ruslanm0502@gmail.com>
2022-07-19 19:14:05 +0200

    v1 character to number conversion.c

---
+++ v2 number to character conversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+   int numero = 0;
+
+   printf(" inserisci il numero da convertire: ");
+   scanf("%d",&numero);
+
+   printf("il carratere è stato convertito in: %c\n",numero);
+
+}
